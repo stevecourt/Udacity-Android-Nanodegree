49007599cbfb2cd2e2bc9e7b51fa35774ebbd6e1
STEVE COURT <court.s@husky.neu.edu>
2015-06-26 12:00:22 -0700

    Finished basic app.
    Finished basic app - does not yet link to any projects.

--- MyAppPortfolio/app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.android.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -14,25 +14,75 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * Called when the user touches the "Spotify Streamer" button
+     */
+    public void spotifyToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my spotify streamer app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * Called when the user touches the "Scores App" button
+     */
+    public void scoresToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my scores app!";
+        int duration = Toast.LENGTH_SHORT;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 
-        return super.onOptionsItemSelected(item);
+    /**
+     * Called when the user touches the "Library App" button
+     */
+    public void libraryToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my library app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Called when the user touches the "Build It Bigger" button
+     */
+    public void biggerToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my build it bigger app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Called when the user touches the "xyz Reader" button
+     */
+    public void readerToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my xyz reader app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Called when the user touches the "Capstone" button
+     */
+    public void capstoneToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
